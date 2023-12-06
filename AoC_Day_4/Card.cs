c9163d4693254d7c10c63a8bef1b5f5cbb0fdca9
Aaron Greiner <aarongreiner@icloud.com>
2023-12-06 19:24:07 +0100

    init day 4 part 1

---
+++ AoC_Day_4/Card.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC_Day_4
+{
+    internal class Card
+    {
+        private List<int> WinningNumbers;
+        private List<int> MyNumbers;
+
+        internal Card(List<int> winningNumbers, List<int> myNumbers)
+        {
+            WinningNumbers = winningNumbers;
+            MyNumbers = myNumbers;
+        }
+
+        internal int GetScore()
+        {
+            int score = 0;
+            int matches = GetMatches();
+
+            for (int i = 0; i < matches; i++)
+            {
+                score = (score == 0) ? 1 : (score == 1) ? 2 : score * 2;
+            }
+
+            return score;
+        }
+
+        internal int GetMatches()
+        {
+            List<int> matches = MyNumbers.Intersect(WinningNumbers).ToList();
+
+            return matches.Count;
+        }
+    }
+}
---
+++ AoC_Day_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC_Day_4
+{
+    internal class Program
+    {
+        private static List<Card> cards = new List<Card>();
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"../../../input.txt");
+
+            int totalScore = 0;
+
+            foreach (string s in input)
+            {
+                cards.Add(new Card(
+                    GetListNumbers(s.Split(':')[1].Split('|')[0]), 
+                    GetListNumbers(s.Split(':')[1].Split('|')[1])));
+            }
+
+            foreach (Card card in cards)
+            {
+                totalScore += card.GetScore();
+            }
+
+            Console.WriteLine("Total points: " + totalScore);
+            Console.WriteLine("Total number of scratchcards: " + "TODO");
+        }
+
+        private static List<int> GetListNumbers(string s)
+        {
+            Regex regex = new Regex(@"\d+");
+            return regex.Matches(s).Cast<Match>().Select(m => int.Parse(m.Value)).ToList();
+        }
+    }
+}
